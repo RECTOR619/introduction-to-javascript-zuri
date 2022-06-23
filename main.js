df39d144cbb636836836c4d13ef74d4bba9cac35
RECTOR619 <olumaintain619@gmail.com>
2022-06-23 18:47:49 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,5 @@
+let myName = "AYANDA JEREMIAH AYOKU";
+let myHeight = "5'7 FEET";
+let myCountry = "NIGERIA";
+
+document.write("Name : " + myName + "<br>" + "Height : " + myHeight + "<br>" + "Country : " + myCountry)
